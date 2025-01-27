caaa144c971cd228f8552cd58b76f3b9c0bed3bb
IkerZaitegi <iker.zaitegi@alumni.mondragon.edu>
2025-01-27 11:59:34 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <csv.h>
 #include <stdio.h>
 #include "point.h"
 #include "pointreader.h"
 #include "angularvelocitycalculator.h"
 
-int main(int argc, char ** argv) {
+int main(int argc, char** argv) {
 	if (argc != 2) {
-		printf("Program to compute maximum angular velocity from a txt file of comma separated values produced by Tracker\n");
-		printf("Usage: %s <filename>\n", argv[0]);
+		printf("Programa para calcular la velocidad angular máxima a partir de un archivo txt de valores separados por comas producido por Tracker\n");
+		printf("Uso: %s <Puntuak9.txt>\n", argv[0]);
 		return -1;
 	}
-	struct point_t* points;
+	struct point_t* points = nullptr;
 
-	int numberOfPoints = readPoints(argv[1], &points);
+ 	int numberOfPoints = readPoints(argv[1], &points); //para saber número de puntos leidos
 
 	if (numberOfPoints < 0) {
-		printf("Error reading file\n");
-		return -1;
+		printf("Error leyendo el archivo\n");
+		return -1;//si el archivo no se lee bien
 	}
 	float maximumAngularVelocity = computeMaximumAngularVelocity(points, numberOfPoints);
-	
-	printf("Maximum angular velocity: %f\n", maximumAngularVelocity);
 
-	freePoints(&points);
+	printf("Velocidad angular maxima: %f\n", maximumAngularVelocity);
+
+	free(points);
 	return 0;
 }
